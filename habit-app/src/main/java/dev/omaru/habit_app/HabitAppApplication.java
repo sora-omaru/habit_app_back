ea1e00cbcedf0c9f0939b404e4e004a00b8c1a11
sora-omaru <konggongxia317@gmail.com>
2025-08-22 17:56:34 +0900

    fix:gitignore整理

---
+++ habit-app/src/main/java/dev/omaru/habit_app/HabitAppApplication.java
@@ -0,0 +1,13 @@
+package dev.omaru.habit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitAppApplication.class, args);
+	}
+
+}
