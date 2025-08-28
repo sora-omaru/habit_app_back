61810acc9e331393dde38ae8ba1acfc7da4ea2da
sora-omaru <konggongxia317@gmail.com>
2025-08-28 15:18:44 +0900

    PageResponse作成

--- src/main/java/dev/omaru/habit_app/repository/HabitRepository.java
+++ src/main/java/dev/omaru/habit_app/repository/HabitRepository.java
@@ -4,9 +4,13 @@ import dev.omaru.habit_app.domain.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
+import  org.springframework.data.domain.Page;
+import  org.springframework.data.domain.Pageable;
+
 public interface HabitRepository extends JpaRepository<Habit,UUID> {
 List<Habit> findByUserId(UUID userId);
     // ユーザーIDで絞り込み＋createdAtの降順ソート
     List<Habit> findByUserIdOrderByCreatedAtDesc(UUID userId);
+    Page<Habit> findByUserId(UUID userId, Pageable pageable);
 }
 
